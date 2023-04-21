ad399d49898c406fcef1838cc025871213527254
Brian Arrieta <brian@spartandevs.com>
2023-04-21 19:32:28 -0400

    [BianArrieta] Practice 4

---
+++ code4.js
@@ -0,0 +1,24 @@
+/* 
+    You get an array of numbers, return the sum of all of the positives ones.
+
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0.
+
+*/
+
+const sumPositiveNumbers = (arr) => {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+      sum += arr[i];
+    }
+  }
+  return sum;
+};
+
+console.log(sumPositiveNumbers([1, 2, 3, 4, 5]));
+console.log(sumPositiveNumbers([1, -2, 3, 4, 5]));
+console.log(sumPositiveNumbers([]));
+console.log(sumPositiveNumbers([-1, -2, -3, -4, -5]));
+console.log(sumPositiveNumbers([-1, 2, 3, 4, -5]));
