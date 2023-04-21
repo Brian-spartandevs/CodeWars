3c3b4a9040f83e9de85afcf372e82f7829675473
Brian Arrieta <brian@spartandevs.com>
2023-04-21 19:17:43 -0400

    [BianArrieta] Practice 3

---
+++ code3.js
@@ -0,0 +1,13 @@
+/* 
+    Multiply
+        This code does not execute properly. Try to figure out why.
+*/
+
+const multiply = (a, b) => {
+  return a * b;
+};
+
+console.log(multiply(1, 1));
+console.log(multiply(2, 1));
+console.log(multiply(2, 2));
+console.log(multiply(3, 5));
