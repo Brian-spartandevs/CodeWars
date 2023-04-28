955b546f5c1fbafa15e89c43dc2a3193d637b777
Brian Arrieta <brian@spartandevs.com>
2023-04-27 21:30:34 -0400

    [BrianArrieta] Practice 21

---
+++ code21.js
@@ -0,0 +1,29 @@
+/* 
+    Highest and Lowest
+    In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+Examples
+
+highAndLow("1 2 3 4 5");  // return "5 1"
+highAndLow("1 2 -3 4 5"); // return "5 -3"
+highAndLow("1 9 3 4 -5"); // return "9 -5"
+
+Notes
+
+    All numbers are valid Int32, no need to validate them.
+    There will always be at least one number in the input string.
+    Output string must be two numbers separated by a single space, and highest number is first.
+
+
+*/
+
+const highestLowest = (str) => {
+  const arr = str.split(" ");
+  arr.sort((a, b) => Number(b) - Number(a));
+  const highest = arr.splice(0, 1);
+  const lowest = arr.splice(-1, 1);
+  return `${highest} ${lowest}`;
+};
+
+console.log(highestLowest("1 2 3 4 5")); // return "5 1"
+console.log(highestLowest("1 2 -3 4 5")); // return "5 -3"
+console.log(highestLowest("1 9 3 4 -5")); // return "9 -5"
