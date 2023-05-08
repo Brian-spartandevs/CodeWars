3379833cf5100a680482be091387a27a1c1c2c17
Brian Arrieta <brian@spartandevs.com>
2023-05-08 08:42:44 -0400

    [BrianArrieta] Practice 34 modified and new practice 35

--- code34.js
+++ code34.js
@@ -1,4 +1,5 @@
 /* 
+    ANALIZO
     Odd-heavy Array
     Array [11,4,9,2,8] is odd-heavy,
 because its odd elements [11,9] are greater than all the even elements [4,2,8]
@@ -14,20 +15,24 @@ Array [3] is odd-heavy,
 because it does not contain any even numbers.
 
 */
-
+//RESUELVO
 //Dividir el array en dos diferentes de numeros de pares y otro de impares
 //extraer el numero mayor de la raiz de pares
 //contar la cantidad de numeros mayores y menores de numeros extraidos
 //si hay mas mayores retorna true si hay mas menores retorna false
 
+//PROGRAMO
 const isOddHeavy = (array) => {
   const arrayEven = array.filter((item) => item % 2 === 0);
   const arrayOdd = array.filter((item) => item % 2 !== 0);
   const evenMax = Math.max(...arrayEven);
+  /* let max = arrayOdd.filter((item)=> item > evenMax).length
+  let min = arrayOdd.filter((item)=> item < evenMax).length
+  return max > min */
   let max = 0;
   let min = 0;
   arrayOdd.forEach((element) => (element > evenMax ? max++ : min++));
-  return max > min ? true : false;
+  return max > min
 };
 
 console.log(isOddHeavy([0, 2, 19, 4, 4]));
