bbf33e648b811c38aa66d28eb438e18c918dd242
Brian Arrieta <brian@spartandevs.com>
2023-07-16 21:04:55 -0400

    [BA] Practice 62

---
+++ code62.js
@@ -0,0 +1,14 @@
+/* Capitalización y mutabilidad
+Se suponía que su compañero de trabajo debía escribir una función auxiliar simple para poner en mayúscula una cadena (que contiene una sola palabra) antes de irse de vacaciones.
+
+Desafortunadamente, ahora se han ido y el código que te dieron no funciona. Corrija la función auxiliar que escribieron para que funcione según lo previsto (es decir, haga que el primer carácter de la cadena "palabra" esté en mayúsculas).
+
+No se preocupe por los números, caracteres especiales o tipos que no son de cadena que se pasan a la función. Las longitudes de las cadenas serán de 1 carácter a 10 caracteres, pero nunca estarán vacías. */
+
+const capitalizeWord = (word) => {
+  return word.replace(word[0], word[0].toUpperCase());
+};
+
+console.log(capitalizeWord("word"));
+console.log(capitalizeWord("pencil"));
+console.log(capitalizeWord("strawberry"));
