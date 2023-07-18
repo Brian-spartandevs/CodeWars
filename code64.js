5b2f55a09939cdcf7599b5a85accd002029b91e5
Brian Arrieta <brian@spartandevs.com>
2023-07-17 22:40:18 -0400

    [BA] Practice 64

---
+++ code64.js
@@ -0,0 +1,9 @@
+/* Este kata se trata de multiplicar un número dado por ocho si es un número par y por nueve de lo contrario. */
+
+const simpleMultiplication = (number) => {
+  if (number % 2 === 0) {
+    return number * 8;
+  } else {
+    return number * 9;
+  }
+};
