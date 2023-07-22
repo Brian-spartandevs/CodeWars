57095913737c51c77b9a93cfd35be1b5715dec51
Brian Arrieta <brian@spartandevs.com>
2023-07-21 23:30:40 -0400

    [BA] Practice 67

---
+++ code67.js
@@ -0,0 +1,10 @@
+/* Orden de lista inverso
+En este kata crearás una función que toma una lista y devuelve una lista con el orden inverso.
+
+Ejemplos (entrada -> salida)
+* [1, 2, 3, 4]  -> [4, 3, 2, 1]
+* [9, 2, 0, 7]  -> [7, 0, 2, 9] */
+
+const reverseList = (list) => {
+  return list.reverse();
+};
