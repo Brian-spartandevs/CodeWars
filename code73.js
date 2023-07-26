8b018b1c4a9d28fec1e39794e2aea7104712b823
Brian Arrieta <brian@spartandevs.com>
2023-07-26 18:23:11 -0400

    [BA] Practice 72 73 and 74

---
+++ code73.js
@@ -0,0 +1,22 @@
+/* Find the divisors!
+DESCRIPTION:
+Create a function named divisors/Divisors that takes an integer n > 1 and returns an array with all of the integer's divisors(except for 1 and the number itself), from smallest to largest. If the number is prime return the string '(integer) is prime' (null in C#, empty table in COBOL) (use Either String a in Haskell and Result<Vec<u32>, String> in Rust).
+
+Example:
+divisors(12); // should return [2,3,4,6]
+divisors(25); // should return [5]
+divisors(13); // should return "13 is prime" */
+
+function divisors(n) {
+  let result = [];
+  for (let i = 2; i <= n / 2; i++) {
+    if (n % i === 0) {
+      result.push(i);
+    }
+  }
+  return result.length ? result : n + " is prime";
+}
+
+console.log(divisors(12));
+console.log(divisors(25));
+console.log(divisors(13));
