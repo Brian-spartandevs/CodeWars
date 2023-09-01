46dce1ed6329d2ea2495647f6e5438fb543b3ae0
Brian Arrieta <brian@spartandevs.com>
2023-09-01 19:53:29 -0400

    [BA] Practices 251 to 260

---
+++ code258.js
@@ -0,0 +1,14 @@
+/* Bit Counting
+Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. You can guarantee that input is non-negative.
+
+Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case */
+
+const countBits = (n) => {
+  return n.toString(2).split("1").length - 1;
+};
+
+console.log(countBits(0));
+console.log(countBits(4));
+console.log(countBits(7));
+console.log(countBits(9));
+console.log(countBits(10));
